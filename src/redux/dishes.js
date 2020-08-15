90015e6b8f76f27334be1b6b464941fbd85ce8ce
Sindhu <sindhukrovvidi@gmail.com>
2020-08-15 17:24:23 +0530

    combining reducers

---
+++ src/redux/dishes.js
@@ -0,0 +1,9 @@
+import { DISHES } from '../shared/dishes';
+
+//educer function takes state and action as parameters
+export const Dishes = (state = DISHES, action) => {
+    switch(action.type) {
+        default:
+            return state
+    }
+}
